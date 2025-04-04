a6419341279a19a9750ffed75692eae30ffff750
shinjith-dev <shinjithpallikkara@gmail.com>
2025-04-05 02:22:50 +0530

    feat: add tequerist & new layout
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -29,7 +29,7 @@ const Template = () => {
               style.section,
               {
                 flexDirection: "row",
-                gap: "32pt",
+                gap: "5%",
                 alignItems: "center",
                 marginBottom: "24pt",
               },
@@ -46,36 +46,36 @@ const Template = () => {
             >
               <Text style={style.textName}>Shinjith P R</Text>
 
-              <Text style={[style.body1, { marginBottom: "6pt" }]}>
+              <Text style={[style.textPost, { marginBottom: "6pt" }]}>
                 Web Developer
               </Text>
 
-              <Text style={style.body3}>
-                Experienced Full Stack Developer, proficient at driving
+              <Text style={style.body}>
+                Experienced React Native Developer, proficient at driving
                 innovation utilizing cutting-edge technology. With over 1½ years
                 of professional experience, I am looking for possibilities for
                 skill development and significant contributions to ideas.
               </Text>
             </View>
 
-            <View style={{ flexDirection: "column", display: "flex" }}>
-              <Link style={style.body2} src="mailto:shinjithdev@gmail.com">
+            <View style={{ flexDirection: "column", display: "flex", gap: '3pt' }}>
+              <Link style={style.body} src="mailto:shinjithdev@gmail.com">
                 shinjithdev@gmail.com
               </Link>
-              <Link style={style.body2} src="tel:+918921345282">
+              <Link style={style.body} src="tel:+918921345282">
                 +91 8921345282
               </Link>
-              <Link style={style.body2} src="https://github.com/shinjith-dev">
+              <Link style={style.body} src="https://github.com/shinjith-dev">
                 github.com/shinjith-dev
               </Link>
-              <Link style={style.body2} src="https://shinjith.dev">
+              <Link style={style.body} src="https://shinjith.dev">
                 shinjith.dev
               </Link>
-              <Text style={style.body2}>Kasaragod, Kerala, India</Text>
+              <Text style={style.body}>Kasaragod, Kerala, India</Text>
             </View>
           </View>
 
-          <View style={[style.section, { flexDirection: "row", gap: "32pt" }]}>
+          <View style={[style.section, { flexDirection: "row", gap: "5%", width: '100%' }]}>
             <View
               style={{
                 flexDirection: "column",
@@ -83,48 +83,38 @@ const Template = () => {
                 flexBasis: "60%",
                 flexShrink: 0,
                 flexGrow: 0,
+                gap: '8pt'
               }}
             >
               {/* Experiences  */}
               <View style={style.section}>
-                <Text style={style.sectionTitle}>Experience</Text>
+                <Text style={style.heading}>Experience</Text>
 
                 <View style={{ marginBottom: "12pt" }}>
-                  <Link src="https://tequerist.com/" style={style.title1}>
+                  <Link src="https://tequerist.com/" style={style.subHeading}>
                     Tequerist Technologies
                   </Link>
-                  <View
-                    style={[
-                      style.body2,
-                      { flexDirection: "row", alignItems: "center" },
-                    ]}
-                  >
-                    <Text style={{ fontWeight: "medium" }}>Developer · </Text>
-                    <Text style={style.body3}>December 2022 - April 2024</Text>
+                  <View style={{ flexDirection: "row", alignItems: "baseline", marginBottom: '3pt' }}>
+                    <Text style={[style.body, { fontWeight: "medium" }]}>Developer · </Text>
+                    <Text style={style.caption}>Spetember 2024 - Present</Text>
                   </View>
-                  <Text style={style.body3}>
-                    As a product-based firm, we are working to scale our product
-                    while also addressing user-reported issues. I was able to
-                    work on older code bases. Furthermore, by scaling service
-                    platforms, I was able to improve my knowledge of mobile app
-                    development and redux systems.
+                  <Text style={style.body}>
+                    I had the chance to collaborate with an amazing team as
+                    a React Native developer. Worked on older code bases and
+                    expanded my knowledge of Redux. I also got the chance to
+                    create a cross-platform booking system for the company's product.
                   </Text>
                 </View>
 
                 <View style={{ marginBottom: "12pt" }}>
-                  <Link src="https://www.tghtech.com/" style={style.title1}>
+                  <Link src="https://www.tghtech.com/" style={style.subHeading}>
                     TGH Tech
                   </Link>
-                  <View
-                    style={[
-                      style.body2,
-                      { flexDirection: "row", alignItems: "center" },
-                    ]}
-                  >
-                    <Text style={{ fontWeight: "medium" }}>Developer · </Text>
-                    <Text style={style.body3}>December 2022 - April 2024</Text>
+                  <View style={{ flexDirection: "row", alignItems: "baseline", marginBottom: '3pt' }}>
+                    <Text style={[style.body, { fontWeight: "medium" }]}>Developer · </Text>
+                    <Text style={style.caption}>December 2022 - April 2024</Text>
                   </View>
-                  <Text style={style.body3}>
+                  <Text style={style.body}>
                     I spent over 1½ year at TGH Tech as a Developer, crafting
                     web interfaces for startups and mid-sized businesses. This
                     role allowed me to master coding techniques and strategic
@@ -133,59 +123,31 @@ const Template = () => {
                 </View>
 
                 <View>
-                  <Link style={style.title1} src="https://www.tghtech.com/">
+                  <Link style={style.subHeading} src="https://www.tghtech.com/">
                     IBM Developer Skills Network
                   </Link>
-                  <View
-                    style={[
-                      style.body2,
-                      { flexDirection: "row", alignItems: "center" },
-                    ]}
-                  >
-                    <Text style={{ fontWeight: "medium" }}>Intern · </Text>
-                    <Text style={style.body3}>November 2022</Text>
+                  <View style={{ flexDirection: "row", alignItems: "baseline", marginBottom: '3pt' }}>
+                    <Text style={[style.body, { fontWeight: "medium" }]}>Intern · </Text>
+                    <Text style={style.caption}>November 2022</Text>
                   </View>
-                  <Text style={style.body3}>
+                  <Text style={style.body}>
                     An internship that covered the fundamentals of web
                     development using Angular, certified by IBM.
                   </Text>
                 </View>
               </View>
 
-              {/* Education  */}
-              <View style={style.section}>
-                <Text style={style.sectionTitle}>Education</Text>
-
-                <View style={{ marginBottom: "12pt" }}>
-                  <Text style={[style.body2, { fontWeight: "medium" }]}>
-                    Bachelor of Technology in Computer Science
-                  </Text>
-                  <Link src="https://lbscek.ac.in/" style={style.title3}>
-                    LBS College of Engineering
-                  </Link>
-                  <Text style={style.body3}>June 2024</Text>
-                </View>
-
-                <View>
-                  <Text style={[style.body2, { fontWeight: "medium" }]}>
-                    Higher studies
-                  </Text>
-                  <Text style={style.title3}>JNV Kasaragod</Text>
-                  <Text style={style.body3}>April 2020</Text>
-                </View>
-              </View>
-
               {/* Projects  */}
               <View style={style.section}>
-                <Text style={style.sectionTitle}>Projects</Text>
+                <Text style={style.heading}>Projects</Text>
                 <View style={{ marginBottom: "12pt" }}>
                   <Link
-                    style={style.title3}
+                    style={[style.subHeading, { marginBottom: '4pt' }]}
                     src="https://github.com/shinjith-dev/academic-ai"
                   >
                     Academic AI
                   </Link>
-                  <Text style={style.body3}>
+                  <Text style={style.body}>
                     An AI assistant that focuses on assigning tasks for students
                     based on the syllabus. It effortlessly{" "}
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
@@ -194,18 +156,41 @@ const Template = () => {
                   </Text>
                 </View>
 
+                <View style={{ marginBottom: "12pt" }}>
+                  <Text
+                    style={[style.subHeading, { marginBottom: '4pt' }]}
+                  >
+                    Blood Donors
+                  </Text>
+                  <Text style={style.body}>
+                    A mobile app created for a public welfare organization.
+                    This software keeps track of blood donors' records and donation histories,
+                    as well as maintaining an up-to-date list monitored by enrolled staff.
+                    Crafted with powerful Expo + Supabase combo.
+                  </Text>
+                </View>
+
+                <View style={{ marginBottom: "12pt" }}>
+                  <Link
+                    style={[style.subHeading, { marginBottom: '4pt' }]}
+                    src="https://pomtick.pages.dev/"
+                  >
+                    Pomtick
+                  </Link>
+                  <Text style={style.body}>
+                    Pomtick is an elegant themeable pomodoro timer that works on multiple platforms.  It is powered by Turborepo and comes with a native program (Tauri) for Windows, Linux, and MacOS, as well as a web application (Next.js).
+                  </Text>
+                </View>
+
                 <View>
                   <Link
-                    style={style.title3}
-                    src="https://movie-loft.netlify.app/"
+                    style={[style.subHeading, { marginBottom: '4pt' }]}
+                    src="https://shinjith.dev/"
                   >
-                    Movie Loft
+                    shinjith.dev
                   </Link>
-                  <Text style={style.body3}>
-                    Movie Loft is a web-based movie database client with a
-                    minimal user interface. It can be useful to obtain
-                    information about movies, television shows, and their
-                    specifics.
+                  <Text style={style.body}>
+                    A portfolio website developed with Nextjs and Tailwindcss.  Designed with an appealing modern UI, smooth scrolling effect, and much more.
                   </Text>
                 </View>
               </View>
@@ -218,66 +203,90 @@ const Template = () => {
                 flexGrow: 0,
                 flexShrink: 0,
                 flexWrap: "wrap",
-                flexBasis: "34%",
+                flexBasis: "35%",
               }}
             >
               {/* Skills  */}
               <View style={style.section}>
-                <Text style={style.sectionTitle}>Skills</Text>
+                <Text style={style.heading}>Skills</Text>
 
                 <View style={{ marginBottom: "12pt" }}>
-                  <Text style={style.title3}>Programming Languages</Text>
-                  <Text style={style.body2}>
-                    JavaScript, TypeScript, C, HTML, CSS, Java, Python, Bash
+                  <Text style={style.subHeading}>Programming Languages</Text>
+                  <Text style={style.body}>
+                    JavaScript, TypeScript, C, Java, Python, Bash
                   </Text>
                 </View>
 
                 <View style={{ marginBottom: "12pt" }}>
-                  <Text style={style.title3}>Libraries & Frameworks</Text>
-                  <Text style={style.body2}>
-                    Next.js, React, React Native, Tauri, Electronjs, NodeJs,
-                    Docusaurus, Material UI, Tailwind, Bootstrap
+                  <Text style={style.subHeading}>Libraries & Frameworks</Text>
+                  <Text style={style.body}>
+                    Nextjs, Expo, React, React Native, Vite,
+                    Tauri, Electronjs, NodeJs, Express,
+                    Material UI, Tailwind, Bootstrap, Sentry
                   </Text>
                 </View>
 
                 <View>
-                  <Text style={style.title3}>Tools & Platforms</Text>
-                  <Text style={style.body2}>
-                    Git, Github, Netlify, Render, AWS Amplify, Cloudflare
+                  <Text style={style.subHeading}>Tools & Platforms</Text>
+                  <Text style={style.body}>
+                    Git, Github, Netlify, Render, AWS, Cloudflare, Docker
                   </Text>
+                </View>
+              </View>
+
+              {/* Education  */}
+              <View style={style.section}>
+                <Text style={style.heading}>Education</Text>
+
+                <View style={{ marginBottom: "12pt", gap: '3pt' }}>
+                  <Text style={style.body}>
+                    Bachelors in CSE
+                  </Text>
+                  <Link src="https://lbscek.ac.in/" style={style.subHeading}>
+                    LBS College of Engineering
+                  </Link>
+                  <Text style={style.caption}>June 2024</Text>
+                </View>
+
+                <View style={{ gap: '3pt' }}>
+                  <Text style={style.body}>
+                    Higher studies
+                  </Text>
+                  <Text style={style.subHeading}>JNV Kasaragod</Text>
+                  <Text style={style.caption}>April 2020</Text>
                 </View>
               </View>
 
               {/* Soft skills */}
               <View style={style.section}>
-                <Text style={style.sectionTitle}>Soft Skills</Text>
+                <Text style={style.heading}>Soft Skills</Text>
 
-                <View style={{ marginTop: "8pt" }}>
+                <View>
                   <Text
                     style={[
-                      style.body2,
-                      { marginBottom: "8pt", fontWeight: "medium" },
+                      style.body,
+                      { marginBottom: "8pt" },
                     ]}
                   >
                     Designing
                   </Text>
                   <Text
                     style={[
-                      style.body2,
-                      { marginBottom: "8pt", fontWeight: "medium" },
+                      style.body,
+                      { marginBottom: "8pt" },
                     ]}
                   >
                     Problem solving
                   </Text>
                   <Text
                     style={[
-                      style.body2,
-                      { marginBottom: "8pt", fontWeight: "medium" },
+                      style.body,
+                      { marginBottom: "8pt" },
                     ]}
                   >
                     Creative thinking
                   </Text>
-                  <Text style={[style.body2, { fontWeight: "medium" }]}>
+                  <Text style={style.body}>
                     Observation
                   </Text>
                 </View>
@@ -285,26 +294,26 @@ const Template = () => {
 
               {/* Language proficiency */}
               <View style={style.section}>
-                <Text style={style.sectionTitle}>Language Proficiency</Text>
+                <Text style={style.heading}>Language Proficiency</Text>
 
-                <View style={{ marginTop: "8pt" }}>
+                <View>
                   <Text
                     style={[
-                      style.body2,
-                      { marginBottom: "8pt", fontWeight: "medium" },
+                      style.body,
+                      { marginBottom: "8pt" }
                     ]}
                   >
                     English
                   </Text>
                   <Text
                     style={[
-                      style.body2,
-                      { marginBottom: "8pt", fontWeight: "medium" },
+                      style.body,
+                      { marginBottom: "8pt" },
                     ]}
                   >
                     Malayalam
                   </Text>
-                  <Text style={[style.body2, { fontWeight: "medium" }]}>
+                  <Text style={style.body}>
                     Hindi
                   </Text>
                 </View>
@@ -312,10 +321,10 @@ const Template = () => {
 
               {/* Interests */}
               <View style={style.section}>
-                <Text style={style.sectionTitle}>Interests</Text>
+                <Text style={style.heading}>Interests</Text>
 
-                <Text style={style.body2}>
-                  Art, traveling, novels & stories, designs, movies
+                <Text style={style.body}>
+                  Art, travel, novels, designs, movies, philosophy
                 </Text>
               </View>
             </View>
